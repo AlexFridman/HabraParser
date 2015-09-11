1b2def8261b7743799d4a55ebaf171b9070bcc48
Егор <egor_metlitsky@mail.ru>
2015-09-11 14:21:25 +0300

    Add HabraArticleParser class

---
+++ HabraMiner/HabraMiner/HabrArticleParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace HabraMiner
+{
+    class HabrArticleParser : IHabrArticleParser
+    {
+        public Article Parse(string data)
+        {
+            Article article = new Article();
+
+            HtmlDocument html = new HtmlDocument();
+            html.LoadHtml(data);
+            HtmlNode node = html.DocumentNode.GetElementByClassName("post shortcuts_item");
+
+
+            return null;
+        }
+
+        
+    }
+
+    public static class HtmlHelpers
+    {
+        public static HtmlNode GetElementByClassName(this HtmlNode node, string className)
+        {
+            return node.SelectNodes($"//*[contains(@class,'{className}')]").FirstOrDefault();
+        }
+        
+    }
+}
