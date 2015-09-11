e0f3afb1e227c94bb58fc5add1c40c95827ba2b3
Alex Fridman <AlexFridman@outlook.com>
2015-09-11 15:22:59 +0300

    Update. Convert Artical to MongoEntity

--- HabraMiner/HabraMiner/Article.cs
+++ HabraMiner/HabraMiner/Article.cs
@@ -1,17 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using HabraMiner.Interfaces;
+using MongoDB.Bson;
 
 namespace HabraMiner
 {
-    public class Article
+    public class Article : IMongoEntity
     {
-        public string Author { get; set; }
-        public string Name { get; set; }
-        public string Company { get; set; }
+        public string Author
+        {
+            get { return InternalDocument["Author"].AsString; }
+            set { InternalDocument["Author"] = value; }
+        }
+
+        public string Name
+        {
+            get { return InternalDocument["Name"].AsString; }
+            set { InternalDocument["Name"] = value; }
+        }
+
+        public string Company
+        {
+            get { return InternalDocument["Company"].AsString; }
+            set { InternalDocument["Company"] = value; }
+        }
+
         public ICollection<string> Habs { get; set; }
-        public DateTime Date { get; set; }
-        public string Text { get; set; }
-        public ICollection<string> CodeComments { get; set; }
-        public int Number { get; set; }
+
+        public DateTime Date
+        {
+            get { return InternalDocument["Date"].ToUniversalTime(); }
+            set { InternalDocument["Company"] = value.ToUniversalTime(); }
+        }
+
+        public string Text
+        {
+            get { return InternalDocument["Text"].AsString; }
+            set { InternalDocument["Text"] = value; }
+        }
+
+        public ICollection<string> CodeComments
+        {
+            get { return InternalDocument["CodeComments"].AsBsonArray.Select(bv => bv.AsString).ToList(); }
+            set { InternalDocument["Text"] = new BsonArray(value); }
+        }
+
+        public ICollection<string> Tags
+        {
+            get { return InternalDocument["Tags"].AsBsonArray.Select(bv => bv.AsString).ToList(); }
+            set { InternalDocument["Tags"] = new BsonArray(value); }
+        }
+
+        public int Number
+        {
+            get { return InternalDocument["Number"].AsInt32; }
+            set { InternalDocument["Number"] = value; }
+        }
+
+        public BsonDocument InternalDocument { get; set; }
     }
 }
