655175b39394d22f0f2b744d7f24ea29afe29c14
Alex Fridman <AlexFridman@outlook.com>
2015-09-11 13:21:33 +0300

    Fix. PageLoader. Call _taskPostProcessor surrounded with try/catch

--- HabraMiner/HabraMiner/PageLoader.cs
+++ HabraMiner/HabraMiner/PageLoader.cs
@@ -10,26 +10,26 @@ namespace HabraMiner
 {
     public class PageLoader
     {
-        private readonly Queue<Task<string>> _taskQueue;
-        private readonly Action<string> _taskPostProcessor;
+        private readonly Queue<Task<Tuple<Uri, string>>> _taskQueue;
+        private readonly Action<Tuple<Uri, string>> _taskPostProcessor;
         private readonly string _userAgent;
 
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-        public PageLoader(IEnumerable<Uri> uris,Action<string> taskPostProcessor, string userAgent)
+        public PageLoader(IEnumerable<Uri> uris,Action<Tuple<Uri, string>> taskPostProcessor, string userAgent)
         {
             _taskQueue = FormTaskQueue(uris);
             _taskPostProcessor = taskPostProcessor;
             _userAgent = userAgent;
         }
 
-        private Queue<Task<string>> FormTaskQueue(IEnumerable<Uri> uris)
+        private Queue<Task<Tuple<Uri, string>>> FormTaskQueue(IEnumerable<Uri> uris)
         {
-            var tasks = uris.Select(uri => new Task<string>(DownloadPage, uri));
+            var tasks = uris.Select(uri => new Task<Tuple<Uri, string>>(DownloadPage, uri));
 
-            return new Queue<Task<string>>(tasks);
+            return new Queue<Task<Tuple<Uri, string>>>(tasks);
         }
 
-        private string DownloadPage(object arg)
+        private Tuple<Uri, string> DownloadPage(object arg)
         {
             var uri = (Uri) arg;
 
@@ -40,7 +40,7 @@ namespace HabraMiner
                 var page = client.DownloadString(uri);
                 Logger.Info($"Downloaded {uri.AbsolutePath}");
 
-                return page;
+                return Tuple.Create(uri, page);
             }
             catch (Exception ex)
             {
@@ -61,16 +61,23 @@ namespace HabraMiner
             }
         }
 
-        private void TaskPostProcessing(Task<string> task)
+        private void TaskPostProcessing(Task<Tuple<Uri, string>> task)
         {
             if (task.Exception != null)
             {
                 return;
             }
 
-            var result = task.Result;
+            
 
-            _taskPostProcessor(result);
+            try
+            {
+                _taskPostProcessor(task.Result);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Unsuccessful post processing {task.Result.Item1.AbsolutePath}");
+            }
         }
     }
 }
