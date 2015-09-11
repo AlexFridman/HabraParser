e3ba1913a60b083f4449dd313b914f431ae5b599
Alex Fridman <AlexFridman@outlook.com>
2015-09-11 23:18:38 +0300

    Update. Some refactoring of HabrArticleParser

--- HabraMiner/HabraMiner/HabrArticleParser.cs
+++ HabraMiner/HabraMiner/HabrArticleParser.cs
@@ -1,46 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using HabraMiner.Interfaces;
 using HtmlAgilityPack;
 
 namespace HabraMiner
 {
-    class HabrArticleParser : IHabrArticleParser
+    internal class HabrArticleParser : IHabrArticleParser
     {
         public Article Parse(string data)
         {
-            Article article = new Article();
+            var html = ConstructHtmlDocument(data);
 
-            HtmlDocument html = new HtmlDocument();
-            html.LoadHtml(data);
-            HtmlNode articleNode = html.DocumentNode.GetElementByClassName("post shortcuts_item");
+            var articleNode = html.DocumentNode.GetElementByClassName("post shortcuts_item");
 
-            article.Name = articleNode.GetElementByClassName("post_title").InnerText;
-
-            article.Date = HtmlHelpers.ParseHabrFormatDate(articleNode.GetElementByClassName("published").InnerText);
-            
-            article.Habs = new List<string>();
-            var habs = articleNode.GetElementByClassName("hubs").ChildNodes.Where(n => n.Name == "a");
-            foreach (var hab in habs)
+            var article = new Article
             {
-                article.Habs.Add(hab.InnerText);
-            }
+                Name = ExtractName(articleNode),
+                Date = ExtractDate(articleNode),
+                Habs = ExtractHabs(articleNode),
+                Rating = ExtractRating(articleNode),
+                Views = ExtractView(articleNode),
+                Favourites = ExtractFavourites(articleNode)
+            };
 
+
+            // TODO parse tags
             article.Tags = new List<string>();
             var tags = articleNode.GetElementByClassName("tags").ChildNodes.First(n => n.Name == "li")
                 .ChildNodes.First(n => n.Name == "a");
 
-            article.Rating = int.Parse(articleNode.GetElementByClassName("voting-wjt__counter-score js-score").InnerText);
-            article.Views = int.Parse(articleNode.GetElementByClassName("views-count_post").InnerText);
-            article.Favourites = int.Parse(articleNode.GetElementByClassName("favorite-wjt__counter js-favs_count").InnerText);
-
             // TODO add author props
             article.Author = articleNode.GetElementByClassName("author-info__name").InnerText;
-            
+
             // this is html, not text
             // TODO parse Text
             article.Text = articleNode.GetElementByClassName("content html_format").InnerHtml;
@@ -48,6 +40,42 @@ namespace HabraMiner
             return null;
         }
 
-        
+        private static int ExtractFavourites(HtmlNode articleNode)
+        {
+            return int.Parse(articleNode.GetElementByClassName("favorite-wjt__counter js-favs_count").InnerText);
+        }
+
+        private static int ExtractView(HtmlNode articleNode)
+        {
+            return int.Parse(articleNode.GetElementByClassName("views-count_post").InnerText);
+        }
+
+        private static int ExtractRating(HtmlNode articleNode)
+        {
+            return int.Parse(articleNode.GetElementByClassName("voting-wjt__counter-score js-score").InnerText);
+        }
+
+        private static string ExtractName(HtmlNode articleNode)
+        {
+            return articleNode.GetElementByClassName("post_title").InnerText;
+        }
+
+        private static DateTime ExtractDate(HtmlNode articleNode)
+        {
+            return HtmlHelpers.ParseHabrFormatDate(articleNode.GetElementByClassName("published").InnerText);
+        }
+
+        private static ICollection<string> ExtractHabs(HtmlNode articleNode)
+        {
+            var habsNodes = articleNode.GetElementByClassName("hubs").ChildNodes.Where(n => n.Name == "a");
+            return habsNodes.Select(hab => hab.InnerText).ToList();
+        }
+
+        private static HtmlDocument ConstructHtmlDocument(string data)
+        {
+            var html = new HtmlDocument();
+            html.LoadHtml(data);
+            return html;
+        }
     }
 }
