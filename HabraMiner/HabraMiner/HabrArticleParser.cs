23ced1a10a18c14e70bbd78bf4a95d83aca91045
Егор <egor_metlitsky@mail.ru>
2015-09-11 15:44:09 +0300

    Add data extract routine in HabrArticleParser

--- HabraMiner/HabraMiner/HabrArticleParser.cs
+++ HabraMiner/HabraMiner/HabrArticleParser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
@@ -14,8 +16,33 @@ namespace HabraMiner
 
             HtmlDocument html = new HtmlDocument();
             html.LoadHtml(data);
-            HtmlNode node = html.DocumentNode.GetElementByClassName("post shortcuts_item");
+            HtmlNode articleNode = html.DocumentNode.GetElementByClassName("post shortcuts_item");
 
+            article.Name = articleNode.GetElementByClassName("post_title").InnerText;
+
+            article.Date = HtmlHelpers.ParseHabrFormatDate(articleNode.GetElementByClassName("published").InnerText);
+            
+            article.Habs = new List<string>();
+            var habs = articleNode.GetElementByClassName("hubs").ChildNodes.Where(n => n.Name == "a");
+            foreach (var hab in habs)
+            {
+                article.Habs.Add(hab.InnerText);
+            }
+
+            article.Tags = new List<string>();
+            var tags = articleNode.GetElementByClassName("tags").ChildNodes.First(n => n.Name == "li")
+                .ChildNodes.First(n => n.Name == "a");
+
+            article.Rating = int.Parse(articleNode.GetElementByClassName("voting-wjt__counter-score js-score").InnerText);
+            article.Views = int.Parse(articleNode.GetElementByClassName("views-count_post").InnerText);
+            article.Favourites = int.Parse(articleNode.GetElementByClassName("favorite-wjt__counter js-favs_count").InnerText);
+
+            // TODO add author props
+            article.Author = articleNode.GetElementByClassName("author-info__name").InnerText;
+            
+            // this is html, not text
+            // TODO parse Text
+            article.Text = articleNode.GetElementByClassName("content html_format").InnerHtml;
 
             return null;
         }
