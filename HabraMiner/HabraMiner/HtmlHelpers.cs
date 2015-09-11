7867c5b836bb5f0cf903323661c497f12e4fe91b
Егор <egor_metlitsky@mail.ru>
2015-09-11 14:49:58 +0300

    Add ParseHabrFormatDate method to HtmlHelpers

--- HabraMiner/HabraMiner/HtmlHelpers.cs
+++ HabraMiner/HabraMiner/HtmlHelpers.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using HtmlAgilityPack;
 
@@ -5,9 +8,19 @@ namespace HabraMiner
 {
     public static class HtmlHelpers
     {
+        public static ICollection<HtmlNode> GetElementsByClassName(this HtmlNode node, string className)
+        {
+            return node.SelectNodes($"//*[contains(@class,'{className}')]");
+        }
+
         public static HtmlNode GetElementByClassName(this HtmlNode node, string className)
         {
-            return node.SelectNodes($"//*[contains(@class,'{className}')]").FirstOrDefault();
+            return node.GetElementsByClassName(className).FirstOrDefault();
+        }
+
+        public static DateTime ParseHabrFormatDate(string dateString)
+        {
+            return DateTime.ParseExact(dateString, "dd MMMM yyyy â hh:mm", new CultureInfo("ru-RU"));
         }
     }
 }
