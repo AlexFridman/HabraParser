0be71b195137cef9d4b07751ffde3daf9e34a3f2
Alex Fridman <AlexFridman@outlook.com>
2015-09-12 22:22:47 +0300

    Add NotParcebleException

---
+++ HabraMiner/HabraMiner/Exceptions/NotParcebleException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabraMiner.Exceptions
+{
+    class NotParcebleException : Exception
+    {
+        public NotParcebleException()
+        {
+        }
+
+        public NotParcebleException(string message) : base(message)
+        {
+        }
+
+        public NotParcebleException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotParcebleException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
