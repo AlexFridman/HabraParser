bf0fea240b926a98fa3aac8b22e19fcde5613aab
Alex Fridman <AlexFridman@outlook.com>
2015-09-12 08:34:59 +0300

    Add PageDownloadTaskFactory

---
+++ HabraMiner/HabraMiner/PageDownloadTasks/PageDownloadTaskFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabraMiner.PageDownloadTasks
+{
+    public class PageDownloadTaskFactory
+    {
+        public const string DefaultUserAgent = "";
+
+        public static PageDownloadTaskBase CreateDownloadTask(Uri uri, Encoding encoding,
+            string userAgent = DefaultUserAgent)
+        {
+            if (IsHabrUri(uri))
+            {
+                return new HabrDownloadTask
+                {
+                    Uri = uri,
+                    DownloadTask = CreateDownloadPageContentTask(uri, encoding, userAgent)
+                };
+            }
+        return new HabrDownloadTask();
+        }
+
+        private static bool IsHabrUri(Uri uri)
+        {
+            return true;
+        }
+
+        private static Task<string> CreateDownloadPageContentTask(Uri uri, Encoding encoding, string userAgent)
+        {
+            return new Task<string>(() =>
+            {
+                var client = new WebClient
+                {
+                    Headers = {["User-Agent"] = userAgent },
+                    Encoding = encoding
+                };
+
+                return client.DownloadString(uri);
+            });
+        }
+    }
+}
