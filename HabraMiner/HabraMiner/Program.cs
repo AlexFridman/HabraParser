5ce350523dca5b7ef4f49da149fb678ca2bfe0a9
Alex Fridman <AlexFridman@outlook.com>
2015-09-12 12:36:52 +0300

    Clean Main procedure

--- HabraMiner/HabraMiner/Program.cs
+++ HabraMiner/HabraMiner/Program.cs
@@ -9,17 +9,6 @@ namespace HabraMiner
     {
         private static void Main(string[] args)
         {
-            var tasks = new[]
-            {
-                PageDownloadTaskFactory.CreateDownloadTask(new Uri("http://www.google.com"), Encoding.ASCII, "", 1000),
-                PageDownloadTaskFactory.CreateDownloadTask(new Uri("http://www.google.com"), Encoding.ASCII, "", 2000),
-                PageDownloadTaskFactory.CreateDownloadTask(new Uri("http://www.google.com"), Encoding.ASCII, "", 3000)
-            };
-
-            var loader = new PageLoader(tasks.ToList(), dto => { });
-            loader.RunAllDellayedTasks();
-
-            Thread.CurrentThread.Join();
         }
     }
 }
