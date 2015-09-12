5c2ec6c925a7483d0201c5ac9408f591633c9463
Alex Fridman <AlexFridman@outlook.com>
2015-09-12 13:25:45 +0300

    Add IArticleServer and its implementation

---
+++ HabraMiner/HabraMiner/IArticleSaver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HabraMiner.Articles;
+
+namespace HabraMiner
+{
+    public interface IArticleSaver<TArticle>
+    {
+        void Save(TArticle article);
+    }
+}
---
+++ HabraMiner/HabraMiner/MongoArticleSaver.cs
@@ -0,0 +1,21 @@
+﻿using HabraMiner.Articles;
+using MongoDB.Driver;
+
+namespace HabraMiner
+{
+    public class MongoArticleSaver<TArticle> : IArticleSaver<TArticle>
+    {
+        private readonly IMongoCollection<TArticle> _collection;
+
+        public MongoArticleSaver(string connectionString, string databaseName, string collectionName)
+        {
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+            _collection = database.GetCollection<TArticle>(collectionName);
+        }
+        public void Save(TArticle article)
+        {
+            _collection.InsertOneAsync(article);
+        }
+    }
+}
