ee46be677866cff766a08055f29934a095f52fb1
Alex Fridman <AlexFridman@outlook.com>
2015-09-12 22:24:17 +0300

    Rename NotParcebleException to NotParcebleArticleException

---
+++ HabraMiner/HabraMiner/Exceptions/NotParcebleArticleException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabraMiner.Exceptions
+{
+    class NotParcebleArticleException : Exception
+    {
+        public NotParcebleArticleException()
+        {
+        }
+
+        public NotParcebleArticleException(string message) : base(message)
+        {
+        }
+
+        public NotParcebleArticleException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotParcebleArticleException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
