670264095c98215df68e875fc06afb1cad6ba5eb
Alex Fridman <AlexFridman@outlook.com>
2015-09-13 11:03:40 +0300

    Fix. Now returns empty collections instead of NULL

--- HabraMiner/HabraMiner/HtmlHelpers.cs
+++ HabraMiner/HabraMiner/HtmlHelpers.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
-using NLog.LayoutRenderers;
 
 namespace HabraMiner
 {
