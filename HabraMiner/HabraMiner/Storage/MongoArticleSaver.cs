e1434a63fccbcfdcefdd61adff249301a1c6becb
Alex Fridman <AlexFridman@outlook.com>
2015-09-13 13:19:06 +0300

    Update. Add batch save to MongoSaver

--- HabraMiner/HabraMiner/Storage/MongoArticleSaver.cs
+++ HabraMiner/HabraMiner/Storage/MongoArticleSaver.cs
@@ -1,4 +1,8 @@
-﻿using HabraMiner.Articles;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HabraMiner.Articles;
 using MongoDB.Driver;
 using NLog;
 
@@ -8,18 +12,57 @@ namespace HabraMiner.Storage
     {
         private readonly MongoCollection<TArticle> _collection;
         private static Logger Logger = LogManager.GetLogger("Saver");
+        private readonly ConcurrentQueue<TArticle> _articlesToSaveQueue = new ConcurrentQueue<TArticle>();
+        private readonly int _batchCount;
 
-        public MongoArticleSaver(string adress, int port, string databaseName, string collectionName)
+        public MongoArticleSaver(string adress, int port, string databaseName, string collectionName, int batchCount = 50)
         {
             var settings = new MongoServerSettings {Server = new MongoServerAddress(adress, port)};
             var server = new MongoServer(settings);
             var database = server.GetDatabase(databaseName);
             _collection = database.GetCollection<TArticle>(collectionName);
+            _batchCount = batchCount;
         }
         public void Save(TArticle article)
         {
-            _collection.Save(article);
-            Logger.Info($"Saved {article.Uri.AbsolutePath}");
+            _articlesToSaveQueue.Enqueue(article);
+            if (_articlesToSaveQueue.Count > _batchCount)
+            {
+                lock (_articlesToSaveQueue)
+                {
+                    var articlesToSave = GetBatch(_articlesToSaveQueue, _batchCount);
+                    Task.Run(() => SaveBatch(articlesToSave));
+                }
+            }
+            //Logger.Info($"Added to save queue {article.Uri.AbsolutePath}");
         }
+
+        private ICollection<TArticle> GetBatch(ConcurrentQueue<TArticle> articles, int count)
+        {
+            var result = new List<TArticle>();
+            var i = 0;
+            while (i < count && !articles.IsEmpty)
+            {
+                TArticle article;
+                articles.TryDequeue(out article);
+                result.Add(article);
+
+                i++;
+            }
+            return result;
+        }
+
+        public void SaveBatch(ICollection<TArticle> articles)
+        {
+            _collection.InsertBatch(articles);
+            Logger.Info($"Batch saved ({_batchCount}). First {articles.First().Uri.AbsolutePath}");
+        }
+
+        public void Flush()
+        {
+            SaveBatch(_articlesToSaveQueue.ToList());
+        }
+
+       
     }
 }
